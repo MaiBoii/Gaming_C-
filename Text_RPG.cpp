56e07542e9ef45361552c2ba8e3a21a42e791325
MaiBoii <20183010@gs.cwnu.ac.kr>
2023-03-11 01:23:28 +0900

    cls

--- Text_RPG.cpp
+++ Text_RPG.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 
-int main() {
+enum MENU 
+{
+    MM_MAP = 1,
+    MM_STORE,
+    MM_INVENTORY,
+    MM_EXIT,
+};
 
+int main() {
+    while(true)
+    {
+        system("cls");
+        cout << "선택지를 정해주십시오." << endl;
+        cout << "1. 지도 열기" << endl;
+        cout << "2. 상점 열기" << endl;
+        cout << "3. 가방 열기" << endl;
+        cout << "4. 게임 종료" << endl;
+        break;
+    }
     return 0;
 }
