156e270dc239d5358a22707b549b4876f06bde3f
MaiBoii <msu2020@naver.com>
2023-03-12 16:00:36 +0900

    map

--- Text_RPG.cpp
+++ Text_RPG.cpp
@@ -13,9 +13,10 @@ enum MENU
 
 enum MAP
 {
-    MM_RIVERWOOD = 1,
-    MM_WHITERUN,
-    MM_SOLITUDE,
+    MM_VILLAGE = 1,
+    MM_MOUNTAIN,
+    MM_DUNGEON,
+    MM_DEVIL,
     MM_BACK,
 };
 
@@ -41,6 +42,7 @@ struct _tagChar
 {
     char strName[NAME_SIZE];
     char jobName[NAME_SIZE];
+    char raceName[NAME_SIZE];
     JOB eJOB;
     RACE eRace;
     int minAttack;
@@ -59,7 +61,6 @@ struct _tagChar
 struct _tagMonster
 {
     char strName[NAME_SIZE];
-    RACE eRace;
     int minAttack;
     int maxAttack;
     int minDefence;
@@ -84,6 +85,48 @@ int main() {
     cout << "=> ";
     cin.getline(tPlayer.strName, NAME_SIZE-1);
 
+    int iRACE = 0;
+    while(iRACE == 0)
+    {
+        cout << "종족을 선택해 주세요." <<endl;
+            cout << "1. 인간" << endl;
+            cout << "2. 오크" << endl;
+            cout << "3. 엘프" << endl;
+            cout << "4. 드워프" << endl;
+            cout << "5. 마족" <<endl;
+            cout << "=> ";
+            cin >> iRACE;
+
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1024, '\n');
+                continue;
+            }
+
+            else if(iRACE <= 0 || iRACE > RACE_DEVIL) 
+            {   
+                system("clear");
+                cout << "잘못된 선택지입니다." <<endl;
+                iRACE = 0;
+            } 
+
+            tPlayer.eRace = (RACE)iRACE;
+            switch(tPlayer.eRace)
+            {
+                case RACE_HUMAN:
+                    strcpy(tPlayer.raceName, "인간");
+                case RACE_OAK:
+                    strcpy(tPlayer.raceName, "오크");
+                case RACE_ELF:
+                    strcpy(tPlayer.raceName, "엘프");
+                case RACE_DWARF:
+                    strcpy(tPlayer.raceName, "드워프");
+                case RACE_DEVIL:
+                    strcpy(tPlayer.raceName, "마족");  
+            };
+    }
+
     int iJOB = 0;
     while(iJOB == 0)
     {
@@ -92,7 +135,7 @@ int main() {
             cout << "2. 궁수" << endl;
             cout << "3. 마법사" << endl;
             cout << "4. 도적" << endl;
-            cout << "5. 개닝기미씨발버러지무직백수(30)" <<endl;
+            cout << "5. 개닝기미씨발줟버러지무직백수" <<endl;
             cout << "=> ";
             cin >> iJOB;
 
@@ -173,23 +216,55 @@ int main() {
                     tPlayer.currentMP = 30000;
                     break;   
             }
+            _tagMonster tMonsterArr[5] = {};
 
-    _tagMonster tMonsterArr[MT_BACK - 1] = {}
+            strcpy(tMonsterArr[0].strName, "늑대");
+            tMonsterArr[0].minAttack = 10;
+            tMonsterArr[0].maxAttack = 20;
+            tMonsterArr[0].minDefence = 10;
+            tMonsterArr[0].maxDefence = 20;
+            tMonsterArr[0].maxHP = 100;
+            tMonsterArr[0].currentHP = 100;
+            tMonsterArr[0].maxMP = 20;
+            tMonsterArr[0].currentMP = 20;
 
-    strcpy(tMonsterArr[0].strName, "고블린");
-    
+            strcpy(tMonsterArr[1].strName, "고블린");
+            tMonsterArr[1].minAttack = 10;
+            tMonsterArr[1].maxAttack = 20;
+            tMonsterArr[1].minDefence = 10;
+            tMonsterArr[1].maxDefence = 20;
+            tMonsterArr[1].maxHP = 100;
+            tMonsterArr[1].currentHP = 100;
+            tMonsterArr[1].maxMP = 20;
+            tMonsterArr[1].currentMP = 20;
+
+            strcpy(tMonsterArr[2].strName, "트롤");
+            tMonsterArr[2].minAttack = 10;
+            tMonsterArr[2].maxAttack = 20;
+            tMonsterArr[2].minDefence = 10;
+            tMonsterArr[2].maxDefence = 20;
+            tMonsterArr[2].maxHP = 100;
+            tMonsterArr[2].currentHP = 100;
+            tMonsterArr[2].maxMP = 20;
+            tMonsterArr[2].currentMP = 20;
 
 
-
+            strcpy(tMonsterArr[4].strName, "대마왕");
+            tMonsterArr[4].minAttack = 1000;
+            tMonsterArr[4].maxAttack = 2000;
+            tMonsterArr[4].minDefence = 1000;
+            tMonsterArr[4].maxDefence = 2000;
+            tMonsterArr[4].maxHP = 10000;
+            tMonsterArr[4].currentHP = 10000;
+            tMonsterArr[4].maxMP = 2000;
+            tMonsterArr[4].currentMP = 2000;
     }
 
     while(true)
     {
-
-
         system("clear");
-        cout << "          환영합니다." << tPlayer.strName << "님!\n" << endl;
-        cout << "======== 선택지를 정해주십시오. ========" << endl;
+        cout << "             환영합니다." << tPlayer.strName << "님!\n" << endl;
+        cout << "================ 선택지를 정해주십시오. ================" << endl;
         cout << "1. 지도 열기" << endl;
         cout << "2. 상점 열기" << endl;
         cout << "3. 가방 열기" << endl;
@@ -217,11 +292,12 @@ int main() {
             {
                 while(true){
                     system("clear");
-                    cout << "======== 마을 간 이동 ========" << endl;
-                    cout << "1. 리버우드" << endl;
-                    cout << "2. 화이트런" << endl;
-                    cout << "3. 솔리튜드" << endl;
-                    cout << "4. 뒤로가기" << endl;
+                    cout << "================ 마을 간 이동 ================" << endl;
+                    cout << "1. 마을(쉬움)" << endl;
+                    cout << "2. 산골(중간)" << endl;
+                    cout << "3. 던전(어려움)" << endl;
+                    cout << "4. 마계(극악)" << endl;
+                    cout << "5. 뒤로 가기" << endl;
                     cout << "=> ";
                     cin >> iMenu;
 
@@ -236,6 +312,29 @@ int main() {
                     {   
                         break;
                     }
+
+                    _tagMonster tMonster = tMonsterArr[iMenu - 1];
+                    while(true)
+                    {
+                        switch(iMenu)
+                        {
+                            case MM_VILLAGE:
+                                cout << " ============== 마을 ============== " << endl;
+                                break;
+                            case MM_MOUNTAIN:
+                                cout << " ============== 산골 ============== " << endl;
+                                break;
+                            case MM_DUNGEON:
+                                cout << " ============== 던전 ============== " << endl;
+                                break;
+                            case MM_DEVIL:
+                                cout << " ============== 마왕 ============== " << endl;
+                                break;
+                        }
+
+                        cout << ""
+                    }
+
                     break;
                 }
                 break;
